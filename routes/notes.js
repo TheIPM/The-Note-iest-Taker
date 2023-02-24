df388ab68237ba34773513e391455c358fdfa456
TheIPM <islamluse@gmail.com>
2023-02-24 19:34:42 +1030

    able to write notes in it

--- routes/notes.js
+++ routes/notes.js
@@ -2,19 +2,22 @@ const fs = require('fs');
 const router = require('express').Router();
 let db = require('../db/db.json');
 
-router.get('/', (req, res) => {
-    res.json(db);
+router.get('/notes', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.json(db);
 });
 
 router.post('/notes', (req, res) => {
-  const newNote = req.body;
-  newNote.id = Math.floor(Math.random() * 1000000);
-  db.push(newNote);
-  fs.writeFile('./db/db.json', JSON.stringify(db), err => {
-    if (err) throw err;
-    res.json(newNote);
+    console.log(req.body);
+    const newNote = req.body;
+    newNote.id = Math.floor(Math.random() * 1000000);
+    db.push(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(db), err => {
+      if (err) throw err;
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.json(newNote);
+    });
   });
-});
 
 router.delete('/notes/:id', (req, res) => {
   const id = parseInt(req.params.id);
@@ -23,9 +26,11 @@ router.delete('/notes/:id', (req, res) => {
     db.splice(index, 1);
     fs.writeFile('./db/db.json', JSON.stringify(db), err => {
       if (err) throw err;
+      res.setHeader('Access-Control-Allow-Origin', '*');
       res.sendStatus(200);
     });
   } else {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.sendStatus(404);
   }
 });
--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
+app.use(express.json()); 
+
+
+app.use((req, res, next) => {
+  console.log(req.body);
+  next();
+});
 
 const notesRouter = require('./routes/notes');
 app.use('/api', notesRouter);
